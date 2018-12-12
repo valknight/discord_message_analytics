44c2dbcddcd3285c1f976840e55e192dd4ce3273
Valerokai <vallerie.knight@gmail.com>
2018-12-12 16:53:40 +0000

    Remove loading from admin, and replace with a loader

--- gssp_experiments/cogs/admin.py
+++ gssp_experiments/cogs/admin.py
@@ -14,70 +14,15 @@ from gssp_experiments.database.database_tools import DatabaseTools, insert_role,
 from gssp_experiments.role_c import DbRole
 from gssp_experiments.settings.config import config, strings
 from gssp_experiments.utils import get_role
-
-startup_extensions = [
-    "gssp_experiments.cogs.controls",
-    "gssp_experiments.cogs.markov",
-    "gssp_experiments.cogs.sentiment",
-    "gssp_experiments.cogs.slurs",
-    "gssp_experiments.cogs.nyoom",
-    "gssp_experiments.cogs.tagger",
-    "gssp_experiments.cogs.fun",
-    "gssp_experiments.cogs.ping",
-    "gssp_experiments.cogs.unembed"
-]
+from gssp_experiments.logger import logger
 
 
 class Admin():
 
     def __init__(self, client):
-        self.automated = subprocess.Popen([sys.executable, "automated_messages.py"])
         self.client = client
         self.database_tools = DatabaseTools(client)
         self.client_tools = ClientTools(client)
-        for extension in startup_extensions:
-            try:
-                client.load_extension(extension)
-            except Exception as e:
-                exc = '{}: {}'.format(type(e).__name__, e)
-                print('Failed to load extension {}\n{}'.format(extension, exc))
-
-    @commands.is_owner()
-    @commands.command()
-    async def unload(self, ctx, extension_name: str):
-        """Unloads an extension."""
-        self.client.unload_extension(extension_name)
-        await ctx.send("{} unloaded.".format(extension_name))
-
-    @commands.is_owner()
-    @commands.command()
-    async def load(self, ctx, extension_name: str):
-        """Loads an extension. """
-        self.client.load_extension(extension_name)
-        await ctx.send("{} loaded.".format(extension_name))
-
-    @is_owner_or_admin()
-    @commands.command()
-    async def reload(self, ctx):
-        """Reload all existing cogs"""
-
-        em = discord.Embed(title = "Killing automated bot", color = red)
-        output = await ctx.channel.send(embed = em)
-        self.automated.kill()
-
-        reload_text = "Reloading {}"
-        startup_extensions_temp = startup_extensions
-        startup_extensions_temp.insert(0, "gssp_experiments.cogs.admin")
-
-        em = discord.Embed(title = "Reloading - please wait", color = red)
-        await output.edit(embed = em)
-
-        for cog in startup_extensions_temp:
-            self.client.unload_extension(cog)
-            self.client.load_extension(cog)
-
-        em = discord.Embed(title = "Finished!", colour = green)
-        await output.edit(embed = em)
 
     @commands.is_owner()
     @commands.command()
@@ -88,25 +33,27 @@ class Admin():
         for channel in ctx.guild.text_channels:
             # we run through every channel as discord doesn't provide an
             # easy alternative
-            print(str(channel.name) + " is being processed. Please wait.")
-            async for message in channel.history(limit = None, reverse = True):
+            logger.info(str(channel.name) +
+                        " is being processed. Please wait.")
+            async for message in channel.history(limit=None, reverse=True):
                 try:
                     cursor.execute(add_message,
                                    (int(message.id), str(ctx.channel.id),
                                     message.created_at.strftime('%Y-%m-%d %H:%M:%S')))
                 except mysql.connector.errors.DataError:
-                    print("Couldn't insert {} - likely a time issue".format(message.id))
+                    logger.info(
+                        "Couldn't insert {} - likely a time issue".format(message.id))
                 except mysql.connector.errors.IntegrityError:
                     pass
             # commit is here, as putting it in for every message causes
             # mysql to nearly slow to a halt with the amount of queries
             cnx.commit()
             print(str(channel.name) + " has been processed.")
-        print("Server {} processing completed".format(str(ctx.guild)))
+        logger.info("Server {} processing completed".format(str(ctx.guild)))
 
     @is_owner_or_admin()
     @commands.command()
-    async def is_processed(self, ctx, user = None):
+    async def is_processed(self, ctx, user=None):
         """
         Admin command used to check if a member has opted in
         """
@@ -114,7 +61,7 @@ class Admin():
             user = ctx.author.name
 
         await ctx.send(strings['process_check']['status']['checking'])
-        if not self.database_tools.opted_in(user = user):
+        if not self.database_tools.opted_in(user=user):
             return await ctx.send(strings['process_check']['status']['not_opted_in'])
         await ctx.send(strings['process_check']['status']['opted_in'])
         return
@@ -144,8 +91,8 @@ class Admin():
 
         """
         cnx = mysql.connector.connect(**config['mysql'])
-        cursor = cnx.cursor(dictionary = True)
-        table_name = self.database_tools.opted_in(user_id = ctx.author.id)
+        cursor = cnx.cursor(dictionary=True)
+        table_name = self.database_tools.opted_in(user_id=ctx.author.id)
 
         query_users = "SELECT user_id, username FROM `gssp_logging`.`users` WHERE opted_in = 1"
         cursor.execute(query_users)
@@ -155,7 +102,8 @@ class Admin():
         drop = "DROP TABLE `gssp_logging`.`%s`;"
         for user in users:
             try:
-                cursor.execute(query, (self.database_tools.opted_in(user_id = user['user_id']),))
+                cursor.execute(
+                    query, (self.database_tools.opted_in(user_id=user['user_id']),))
                 messages = cursor.fetchall()
                 await ctx.send("Combining " + user['username'])
                 for message in messages:
@@ -168,7 +116,8 @@ class Admin():
                 cnx.commit()
                 await ctx.send("Inserted %s for %s" % (len(messages), user['username']))
                 try:
-                    cursor.execute(drop, (self.database_tools.opted_in(user_id = user['user_id']),))
+                    cursor.execute(
+                        drop, (self.database_tools.opted_in(user_id=user['user_id']),))
                 except:
                     pass
             except:
@@ -254,11 +203,10 @@ class Admin():
                     member_ids = []
                     for member in role.members:
                         member_ids.append(member.id)
-                    role_db = DbRole(role.id, role.name, 0, members = member_ids)
+                    role_db = DbRole(role.id, role.name, 0, members=member_ids)
                     role_db.save_members()
-
-                    print(role.name + " > " + emoji.demojize(role.name))
-                    cursor.execute(update_role, (emoji.demojize(role.name), role.id))
+                    cursor.execute(
+                        update_role, (emoji.demojize(role.name), role.id))
 
 
 def setup(client):
---
+++ gssp_experiments/cogs/loader.py
@@ -0,0 +1,88 @@
+import subprocess
+import sys
+import traceback
+
+import discord
+from discord.ext import commands
+
+from gssp_experiments.checks import is_owner_or_admin
+from gssp_experiments.client_tools import ClientTools
+from gssp_experiments.colours import green, red
+from gssp_experiments.settings.config import config, strings
+from gssp_experiments.logger import logger
+startup_extensions = [
+    "gssp_experiments.cogs.admin",
+    "gssp_experiments.cogs.controls",
+    "gssp_experiments.cogs.markov",
+    "gssp_experiments.cogs.sentiment",
+    "gssp_experiments.cogs.slurs",
+    "gssp_experiments.cogs.nyoom",
+    "gssp_experiments.cogs.tagger",
+    "gssp_experiments.cogs.fun",
+    "gssp_experiments.cogs.ping",
+    "gssp_experiments.cogs.unembed"
+]
+
+
+class Loader():
+    """
+    This short cog is just intended to be a loader for other cogs.
+    Don't add to this unless you have to, as this is designed to be minimal so to prevent breaking all cogs
+    """
+
+    def __init__(self, client):
+        self.automated = subprocess.Popen(
+            [sys.executable, "automated_messages.py"])
+        self.client = client
+        self.client_tools = ClientTools(client)
+        # we look for ".admin" and then add "." to prevent matching a root directory ending in admin
+        path = __name__.replace(".admin", "") + "."
+        for extension in startup_extensions:
+            try:
+                client.load_extension(extension)
+                logger.info("Loaded {}".format(extension.replace(path, "")))
+            except Exception as e:
+                exc = '{}: {}'.format(type(e).__name__, e)
+                logger.error(
+                    'Failed to load extension {}\n{}\n{}'.format(extension, exc, traceback.format_exc()))
+
+    @commands.is_owner()
+    @commands.command()
+    async def unload(self, ctx, extension_name: str):
+        """Unloads an extension."""
+        self.client.unload_extension(extension_name)
+        await ctx.send("{} unloaded.".format(extension_name))
+
+    @commands.is_owner()
+    @commands.command()
+    async def load(self, ctx, extension_name: str):
+        """Loads an extension. """
+        self.client.load_extension(extension_name)
+        await ctx.send("{} loaded.".format(extension_name))
+
+    @is_owner_or_admin()
+    @commands.command()
+    async def reload(self, ctx):
+        """Reload all existing cogs"""
+
+        em = discord.Embed(title="Killing automated bot", color=red)
+        output = await ctx.channel.send(embed=em)
+        self.automated.kill()
+
+        reload_text = "Reloading {}"
+        startup_extensions_temp = startup_extensions
+        startup_extensions_temp.insert(0, "gssp_experiments.cogs.loader")
+
+        em = discord.Embed(title="Reloading - please wait", color=red)
+        await output.edit(embed=em)
+
+        for cog in startup_extensions_temp:
+            self.client.unload_extension(cog)
+            self.client.load_extension(cog)
+
+        em = discord.Embed(title="Finished!", colour=green)
+        await output.edit(embed=em)
+
+
+def setup(client):
+    client.add_cog(Loader(client))
