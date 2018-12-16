b3865bf0bf5077f6c1d646192c7529db717b6937
Valerokai <vallerie.knight@gmail.com>
2018-12-16 11:03:36 +0000

    Add option to customize MySQL port

--- gssp_experiments/database/__init__.py
+++ gssp_experiments/database/__init__.py
@@ -1,7 +1,11 @@
 import mysql.connector
-
+from gssp_experiments.logger import logger
 from gssp_experiments.settings.config import config
 
+port = config['mysql'].get("port")
+if port is None:
+    logger.warn("'mysql'.'port' not found in config - defaulting to 3306")
+    config['mysql']['port'] = 3306
 cnx = mysql.connector.connect(**config['mysql'])
 cursor = cnx.cursor(buffered=True)
-cursor_dict = cnx.cursor(dictionary = True)
+cursor_dict = cnx.cursor(dictionary=True)
