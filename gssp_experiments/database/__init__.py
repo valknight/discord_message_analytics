8b417a6a1458e798a45e5e3b9c13178e04cf5b77
Valerokai <vallerie.knight@gmail.com>
2018-12-16 12:24:31 +0000

    Add error handling when connecting to MySQL

--- gssp_experiments/database/__init__.py
+++ gssp_experiments/database/__init__.py
@@ -1,4 +1,5 @@
 import mysql.connector
+import sys
 from gssp_experiments.logger import logger
 from gssp_experiments.settings.config import config
 
@@ -6,6 +7,11 @@ port = config['mysql'].get("port")
 if port is None:
     logger.warn("'mysql'.'port' not found in config - defaulting to 3306")
     config['mysql']['port'] = 3306
-cnx = mysql.connector.connect(**config['mysql'])
+try:
+    cnx = mysql.connector.connect(**config['mysql'])
+    logger.info("Created connection to MySQL")
+except mysql.connector.errors.InterfaceError as exception:
+    logger.error("{} - check your settings".format(exception))
+    sys.exit(1)
 cursor = cnx.cursor(buffered=True)
 cursor_dict = cnx.cursor(dictionary=True)
