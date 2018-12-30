13b49b2f900e321fa8a32b7cfa76aa7441860143
Val Knight <vallerie.knight@gmail.com>
2018-12-30 18:09:14 +0000

    Add aliases to github

--- gssp_experiments/cogs/fun.py
+++ gssp_experiments/cogs/fun.py
@@ -8,7 +8,7 @@ class Fun():
     def __init__(self, client):
         self.client = client
     
-    @commands.command()
+    @commands.command(aliases=["source", "gitlab", "repo"])
     async def github(self, ctx):
         em = Embed(title="Github link", description="This bot is open source! Check the source, and help development at https://github.com/valknight/discord_message_analytics")
         await ctx.send(embed=em)
