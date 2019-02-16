1069be2b10bbd7627cce0698d5ea86d065d771a3
Vallerie Knight <vallerie.knight@gmail.com>
2019-02-16 15:48:36 +0000

    Give loader access to actual modules of cogs for future, and move to group command

--- gssp_experiments/cogs/loader.py
+++ gssp_experiments/cogs/loader.py
@@ -10,6 +10,7 @@ from gssp_experiments.client_tools import ClientTools
 from gssp_experiments.colours import green, red, blue
 from gssp_experiments.settings.config import config, strings
 from gssp_experiments.logger import logger
+import importlib
 startup_extensions = [
     "admin",
     "controls",
@@ -25,6 +26,7 @@ startup_extensions = [
 ]
 
 
+
 class Loader():
     """
     This short cog is just intended to be a loader for other cogs.
@@ -37,26 +39,43 @@ class Loader():
     def strip_path(self, extension_name):
         path = self.get_path() + "."
         return extension_name.replace(path, "")
-
-    def __init__(self, client):
-        # self.automated = subprocess.Popen(
-        #    [sys.executable, "automated_messages.py"])
-        self.client = client
-        self.client_tools = ClientTools(client)
-        # we look for ".admin" and then add "." to prevent matching a root directory ending in admin
+    
+    def load_all_extensions(self):
+        """
+        This will import all of our extensions, and set extension_imported to the code of the imports
+        """
+        self.extension_imported = []
         for extension in startup_extensions:
             try:
                 to_load = "{}.{}".format(self.get_path(), extension)
-                client.load_extension(to_load)
+                self.extension_imported.append(dict(name=extension, module=importlib.import_module(to_load)))
+                print(to_load)
+                self.client.load_extension(to_load)
                 del(to_load)
                 logger.info("Loaded {}".format(extension))
             except Exception as e:
                 exc = '{}: {}'.format(type(e).__name__, e)
                 logger.error(
                     'Failed to load extension {}\n{}\n{}'.format(extension, exc, traceback.format_exc()))
+        print(self.extension_imported)
     
+    def __init__(self, client):
+        # self.automated = subprocess.Popen(
+        #    [sys.executable, "automated_messages.py"])
+        self.client = client
+        self.client_tools = ClientTools(client)
+        # we look for ".admin" and then add "." to prevent matching a root directory ending in admin
+        
+        self.load_all_extensions()
+    
+    @is_owner_or_admin()
+    @commands.group(aliases=["module"])
+    async def cog(self, ctx):
+        if ctx.invoked_subcommand is None:
+            await ctx.send('Invalid command passed')
+
     @commands.is_owner()
-    @commands.command(aliases=["un_load"])
+    @cog.command(aliases=["un_load"])
     async def unload(self, ctx, extension_name: str):
         """Unloads an extension."""
         to_unload = "{}.{}".format(self.get_path(), extension_name)
@@ -77,15 +96,14 @@ class Loader():
         await ctx.send(embed=em)
 
     @commands.is_owner()
-    @commands.command()
+    @cog.command()
     async def load(self, ctx, extension_name: str):
         """Loads an extension. """
         self.client.load_extension(self.get_path() + "." + extension_name)
         startup_extensions.append(extension_name)
         await ctx.send("{} loaded.".format(extension_name))
 
-    @is_owner_or_admin()
-    @commands.command(aliases=["get_loaded", "getloadedextensions", "get_loaded_extensions", "getloaded"])
+    @cog.command(aliases=["get_loaded", "getloadedextensions", "get_loaded_extensions", "getloaded"])
     async def loaded(self, ctx):
         """Gets loaded extensions. """
         self.client
@@ -99,8 +117,7 @@ class Loader():
                 "- {} \n".format(self.strip_path(extension))
         await ctx.send(embed=em)
 
-    @is_owner_or_admin()
-    @commands.command(aliases=["re_load"])
+    @cog.command(aliases=["re_load"])
     async def reload(self, ctx):
         """Reload all existing cogs"""
         startup_extensions_temp = startup_extensions
@@ -109,14 +126,18 @@ class Loader():
         em = discord.Embed(title="Reloading - please wait", color=red)
         output = await ctx.send(embed=em)
 
+        # before loading any extensions, we need to ensure they are all unloaded first
         for cog in startup_extensions_temp:
             extension_full = "{}.{}".format(self.get_path(), cog)
             self.client.unload_extension(extension_full)
-            self.client.load_extension(extension_full)
+        
+        self.load_all_extensions()
 
         em = discord.Embed(title="Finished!", colour=green)
         await output.edit(embed=em)
-
+        
+        
+    
 
 def setup(client):
     client.add_cog(Loader(client))
