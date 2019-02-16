3c8e76b2f034c4cbff0afb216b423d4c95af4bee
Vallerie Knight <vallerie.knight@gmail.com>
2019-02-16 20:11:18 +0000

    Move bot to sharded system

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 import os
-
+import asyncio
 import discord
 import emoji
 import mysql
@@ -24,89 +24,46 @@ else:
     debug = False
     prefix = config['discord']['prefix']
 
-client = commands.Bot(
-    command_prefix=prefix, owner_id=config['discord']['owner_id'])
+shard_count = config['discord'].get("shard_count")
+if shard_count is None:
+    logger.warn("config['discord']['shard_count'] is not set - defaulting to 1 shard")
+    shard_count = 15
+
+client = commands.AutoShardedBot(
+    command_prefix=prefix, owner_id=config['discord']['owner_id'], shard_count=shard_count)
 
 client_tools = ClientTools(client)
 database_tools = DatabaseTools(client)
 token = config['discord']['token']
 
+@client.event
+async def on_shard_ready(shard_id):
+    print(shard_id)
 
 @client.event
 async def on_ready():
     game = discord.Game("Starting")
     await client.change_presence(activity=game)
-    logger.info("Bot starting. Please wait for synchroization to complete.")
-
-    insert_channel = "INSERT INTO channels (channel_id, channel_name) VALUES (%s, %s)"
-    update_channel = "UPDATE `gssp_logging`.`channels` SET `channel_name`=%s WHERE `channel_id`=%s;"
-
-    members = []
-    # we run this for loop twice so we can seperate scraping of data from core components
-    for guild in client.guilds:
-            guild_settings.add_guild(guild)
+    logger.info("Connected to Discord as {} ({})".format(client.user.name, client.user.id))
     
-    if debug:
-        for guild in client.guilds:
-            logger.info("Found guild {} - {} channels".format(guild.name, len(guild.text_channels)))
-    if not bool(config['discord'].get("skip_scrape")):
-        for guild in client.guilds:
-            logger.info("{}: Updating channels".format(str(guild)))
-            for channel in guild.text_channels:
-                try:
-                    cursor.execute(insert_channel, (channel.id, channel.name))
-                    logger.debug("Inserted {} to DB".format(channel.name))
-                except mysql.connector.errors.IntegrityError:
-                    cursor.execute(update_channel, (channel.name, channel.id))
-                    logger.debug("Updated {}".format(channel.name))
-            logger.info("{}: Updating users".format(str(guild)))
-            for member in guild.members:
-                name = database_tools.opted_in(user_id=member.id)
-                if name is not False:
-                    members.append(member)
-                try:
-                    cursor.execute(insert_users, (member.id,))
-                except mysql.connector.errors.IntegrityError:
-                    pass  # we pass because we just want to make sure we add any new users, so we expect some already here
-                try:
-                    cursor.execute(insert_settings, (member.id,))
-                except mysql.connector.errors.IntegrityError:
-                    pass  # see above
-            logger.info("{}: Finished {} users".format(
-                str(guild), len(guild.members)))
-            logger.info("{}: Updating roles".format(str(guild)))
-            for role in guild.roles:
-                if role.name != "@everyone":
-                    try:
-                        cursor.execute(
-                            insert_role, (role.id, emoji.demojize(role.name), guild.id, int(role.mentionable)))
-                    except mysql.connector.errors.IntegrityError:
-                        cursor.execute(
-                            update_role, (emoji.demojize(role.name), int(role.mentionable), role.id))
-
-                    # this is designed to assist with migration, by moving old discord role members over to the new
-                    # system seamlessly
-                    member_ids = []
-                    for member in role.members:
-                        member_ids.append(member.id)
-                    role_db = DbRole(role.id, role.name, 0, members=member_ids)
-                    role_db.save_members()
-            logger.info("{}: Finished {} roles".format(
-                guild, len(guild.roles)))
-            cnx.commit()
-    else:
-        logger.warn(
-            "Skipping scraping data from existing servers - data may be out of date")
     # This needs to be here, so that all the other cogs can be loaded
     client.load_extension("gssp_experiments.cogs.loader")
     await set_activity(client)
-    logger.info("\n[Connected to Discord]\n[Username]  -   [ {} ]\n[User  ID]  -   [ {} ]".format(
-        client.user.name, client.user.id))
 
-    logger.info(
-        "Initialising building data profiles on existing messages. This will take a while.")
+    for guild in client.guilds:
+            guild_settings.add_guild(guild)
+    members = []
+    for guild in client.guilds:
+        if debug:
+            logger.info("Found guild {} - {} channels".format(guild.name, len(guild.text_channels)))
+        for member in guild.members:
+            name = database_tools.opted_in(user_id=member.id)
+            if name is not False:
+                if name not in members:
+                    members.append(member)
+    logger.info("Initialising building data profiles on existing messages. This will take a while.")
     await client_tools.build_data_profile(members, limit=None)
-
+    
 
 @client.event
 async def on_message(message):
