8a374026895ab3fa854509a7899940692eabc484
Vallerie Knight <vallerie.knight@gmail.com>
2019-02-16 20:11:29 +0000

    Remove role_id from fun.py

--- gssp_experiments/cogs/fun.py
+++ gssp_experiments/cogs/fun.py
@@ -13,12 +13,5 @@ class Fun():
         em = Embed(title="Github link", description="This bot is open source! Check the source, and help development at https://github.com/valknight/discord_message_analytics")
         await ctx.send(embed=em)
     
-    @commands.command(aliases=["role_id"])
-    async def roleid(self, ctx, role_name):
-        for role in ctx.guild.roles:
-            if role_name.lower() == role.name.lower():
-                return await ctx.send(role.id)
-        return await ctx.send(Embed(title="Could not find role {}".format(role_name)))
-
 def setup(client):
     client.add_cog(Fun(client))
