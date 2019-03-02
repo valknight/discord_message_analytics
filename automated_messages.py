3fd174fe8c800669d7e25ca531bdd4a64672da78
Vallerie Knight <vallerie.knight@gmail.com>
2019-03-02 22:04:29 +0000

    Initial patch to try and fix automated_messages

--- automated_messages.py
+++ automated_messages.py
@@ -1,3 +1,4 @@
+import sys
 import concurrent
 
 import discord
@@ -10,9 +11,69 @@ from ags_experiments.database import cnx
 from ags_experiments.database.database_tools import DatabaseTools
 from ags_experiments.settings.config import config, strings
 
-client = commands.Bot(command_prefix="--------------------", owner_id=config['discord']['owner_id'])
+client = commands.Bot(command_prefix="--------------------",
+                      owner_id=config['discord']['owner_id'])
 client_tools = ClientTools(client)
 database_tools = DatabaseTools(client)
+
+print("Awaiting login")
+
+position = None
+opted_in_users = []
+
+async def get_members(server, message=None):
+    members = []
+    for user in server.members:
+        if database_tools.is_automated(user):
+            print(user)
+            members.append(user)
+            if message is not None:
+                await message.edit(content="Initialising - found {count} users".format(count=len(members)))
+    return members
+
+
+async def main():
+    global opted_in_users
+    error_count = 0
+    while True:
+        try:
+            for x in (position, len(opted_in_users)):
+                position = x
+                    
+                messages, channels = await database_tools.get_messages(user.id, config['limit'])
+                cnx.commit()
+                text_full = ""
+
+                for x in range(0, len(messages)):
+                    channel_temp = client.get_channel(int(channels[x]))
+                    if not channel_temp.is_nsfw():
+                        text_full = text_full + messages[x] + "\n"
+                try:
+                    text_model = markovify.NewlineText(
+                        text_full, state_size=config['state_size'])
+                    em = discord.Embed(
+                        title=user.display_name, description=text_model.make_short_sentence(140))
+                    em.set_thumbnail(url=user.avatar_url)
+                    name = await client_tools.get_delete_emoji()
+                    name = name[0]
+                    em.set_footer(
+                        text=strings['markov']['output']['footer'].format(name))
+                    output = await channel.send(embed=em)
+                    time.sleep(1)
+                    async for message in channel.history(limit=1, reverse=True):
+                        message = message
+                        break
+                    await delete_option(client, message, channel, client.get_emoji(int(strings['emojis']['delete'])) or "❌")
+                except KeyError:
+                    pass
+            opted_in_users = await get_members(server)
+            error_count = 0
+            position = 0 # this means that, we don't continue from the *end*
+        except Exception as e:
+            error_count += 1
+            print(e)
+            if error_count > 3:
+                sys.exit(1)
 
 
 @client.event
@@ -25,39 +86,16 @@ async def on_ready():
                 break
         if found:
             break
-
-    while True:
-        try:
-            opted_in_users = []
-            for user in server.members:
-                if database_tools.is_automated(user):
-                    opted_in_users.append(user)
-            for user in opted_in_users:
-                messages, channels = await database_tools.get_messages(user.id, config['limit'])
-                cnx.commit()
-                text_full = ""
-
-                for x in range(0, len(messages)):
-                    channel_temp = client.get_channel(int(channels[x]))
-                    if not channel_temp.is_nsfw():
-                        text_full = text_full + messages[x] + "\n"
-                try:
-                    text_model = markovify.NewlineText(text_full, state_size=config['state_size'])
-                    em = discord.Embed(title=user.display_name, description=text_model.make_short_sentence(140))
-                    em.set_thumbnail(url=user.avatar_url)
-                    name = await client_tools.get_delete_emoji()
-                    name = name[0]
-                    em.set_footer(text=strings['markov']['output']['footer'].format(name))
-                    output = await channel.send(embed=em)
-                    time.sleep(1)
-                    async for message in channel.history(limit=1, reverse=True):
-                        message = message
-                        break
-                    await delete_option(client, message, channel, client.get_emoji(int(strings['emojis']['delete'])) or "❌")
-                except KeyError:
-                    pass
-        except:
-            pass
+        print("Not found")
+    if not found:
+        print("Failed to find channel. Check your config")
+        sys.exit(1)
+    # initial propogation of users
+    message = await channel.send("Starting message loop")
+    global opted_in_users
+    opted_in_users = await get_members(server, message=message)
+    await message.delete()
+    await main()
 
 
 async def delete_option(bot, message, channel, delete_emoji, timeout=config['discord']['delete_timeout'] / 2):
