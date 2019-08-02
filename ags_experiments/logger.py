b2b6b1c9aee6a0d6a4c713c004e5928fe8bdf9c6
tstepka <tstepka@gmail.com>
2019-08-02 19:09:59 -0400

    Change agse_log to discord
    This should actually produce a log file this time, new, and improved, and actually tested in a living breathing bot.

--- ags_experiments/logger.py
+++ ags_experiments/logger.py
@@ -1,8 +1,8 @@
 import logging
-logger = logging.getLogger('agse_log')
-handler = logging.StreamHandler()
+logger = logging.getLogger('discord')
+logger.setLevel(logging.INFO)
+handler = logging.FileHandler(filename='agse.log', encoding='utf-8', mode='w')
 formatter = logging.Formatter("[ %(asctime)s ] [ %(levelname)s ] %(message)s",
                               "%H:%M:%S %d-%m-%Y")
 handler.setFormatter(formatter)
 logger.addHandler(handler)
-logger.setLevel(logging.INFO)
